f70b51e5e7033f4b3abba6e4ac1c9d71e9de1d05
Ashok Kambala <75029215+kambalaashok4@users.noreply.github.com>
2026-08-22 19:04:17 +0530

    Fix AWS credentials loading in DynamoDB integration tests
    - Add AwsCredentialsProvider parameter to dynamoDbClient bean
    - Explicitly set credentials provider on DynamoDbClientBuilder
    - Ensures test credentials from TestAwsCredentialsConfig are properly injected
    - Fixes 'Unable to load credentials' error in integration tests

--- src/cart/src/main/java/com/amazon/sample/carts/config/DynamoDBConfiguration.java
+++ src/cart/src/main/java/com/amazon/sample/carts/config/DynamoDBConfiguration.java
@@ -26,6 +26,7 @@ import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.util.StringUtils;
+import software.amazon.awssdk.auth.credentials.AwsCredentialsProvider;
 import software.amazon.awssdk.enhanced.dynamodb.DynamoDbEnhancedClient;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.dynamodb.DynamoDbClient;
@@ -41,7 +42,10 @@ import software.amazon.awssdk.services.dynamodb.DynamoDbClientBuilder;
 public class DynamoDBConfiguration {
 
   @Bean
-  DynamoDbClient dynamoDbClient(DynamoDBProperties properties) {
+  DynamoDbClient dynamoDbClient(
+    DynamoDBProperties properties,
+    AwsCredentialsProvider credentialsProvider
+  ) {
     log.info("Using DynamoDB persistence");
     log.info("DynamoDB table: {}", properties.getTableName());
 
@@ -53,6 +57,8 @@ public class DynamoDBConfiguration {
       builder.region(Region.US_WEST_2);
       builder.endpointOverride(URI.create(properties.getEndpoint()));
     }
+
+    builder.credentialsProvider(credentialsProvider);
 
     return builder.build();
   }
